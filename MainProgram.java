a602a2677141fdb0db3e62ddfce88b4085f0b817
kurt <kurtmtthw@gmail.com>
2022-08-24 15:38:38 +0800

    Complete Ones and Zeros

--- MainProgram.java
+++ MainProgram.java
@@ -1,20 +1,37 @@
-public class MainProgram { 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    public static void main(String[] args) {
-      
-    }
+public class MainProgram {
+
     /**
      * Checks if inputted integer is a square.
      * @param n integer to check if is perfect square.
      * @return returns true if integer is perfect square otherwise falls.
      */
-    public static boolean isSquare(int n) {   
-      int temp = 0;
+    public static boolean isSquare(int n) {
+        int temp = 0;
         if (n == 2){
             return true;
         } else if (!(n < 0)){
             temp = (int) Math.sqrt(n);
         }
         return ((temp * temp) == n);
+    }
+    // Complete Ones and Zeros (7 kyu)
+    public static int convertBinaryArrayToInt(List<Integer> binary) {
+        int[] binaryEquivalent = {1, 2, 4, 8, 16, 32, 64, 128};
+        int returnVal = 0;
+        int binaryIndex = 0;
+        // 1 2 4 8 16 32 64 128 256
+        for (int i = binary.size(); i-- > 0; ) {
+            if (binary.get(i) == 1) returnVal += binaryEquivalent[binaryIndex];
+            binaryIndex++;
+        }
+        return returnVal;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convertBinaryArrayToInt(new ArrayList<>(Arrays.asList(0, 0, 0, 1))));
     }
 }
