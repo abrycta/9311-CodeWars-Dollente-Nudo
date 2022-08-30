2929eb56d4f48a9ec098b4929da1b0a8f6a785a6
2211922 <2211922@slu.edu.ph>
2022-08-30 20:05:40 +0800

    added isSquare, rpsls, comparePowers, squareDigits, uncompleted menu

--- MainProgram.java
+++ MainProgram.java
@@ -1,21 +1,8 @@
+import java.io.BufferedReader;
 import java.util.*;
 
 public class MainProgram {
 
-    /**
-     * Checks if inputted integer is a square.
-     * @param n integer to check if is perfect square.
-     * @return returns true if integer is perfect square otherwise falls.
-     */
-    public static boolean isSquare(int n) {
-        int temp = 0;
-        if (n == 2){
-            return true;
-        } else if (!(n < 0)){
-            temp = (int) Math.sqrt(n);
-        }
-        return ((temp * temp) == n);
-    }
     // Complete Jaden Casing Strings (7 kyu)
     public static String toJadenCase(String phrase) {
         if (phrase == null || phrase == "") return null;
@@ -88,7 +75,100 @@ public class MainProgram {
         return res;
     }
 
+    /**
+     * Checks if inputted integer is a square. [7 Kyu]
+     * @param n integer to check if is perfect square.
+     * @return returns true if integer is perfect square otherwise falls.
+     */
+    public static boolean isSquare(int n) {
+        int temp = 0;
+        if (n == 2){
+            return true;
+        } else if (!(n < 0)){
+            temp = (int) Math.sqrt(n);
+        }
+        return ((temp * temp) == n);
+    }
+
+    /**
+     * Return the player who won the game. [7 Kyu]
+     * @param player1 player one's input.
+     * @param player2 player two's input.
+     * @return winner of the game.
+     */
+    public static String rpsls(String player1, String player2){
+        if(player1.equals(player2)) return "Draw!";
+        if(player1.equals("scissors") && (player2.equals("paper") || player2.equals("lizard"))) return "Player 1 Won!";
+        if(player1.equals("paper") && (player2.equals("rock") || player2.equals("spock"))) return "Player 1 Won!";
+        if(player1.equals("rock") && (player2.equals("lizard") || player2.equals("scissors"))) return "Player 1 Won!";
+        if(player1.equals("lizard") && (player2.equals("spock") || player2.equals("paper"))) return "Player 1 Won!";
+        if(player1.equals("spock") && (player2.equals("scissors") || player2.equals("rock"))) return "Player 1 Won!";
+        return "Player 2 Won!";
+    }
+
+    /**
+     * Compares the powers of 2 numbers, returning -1 if the first member is larger,
+     * 0 if they are equal, otherwise will return 1. [6 Kyu]
+     * @param number1 first number.
+     * @param number2 second number.
+     * @return Return -1 if the first member is larger,
+     *  0 if they are equal, otherwise return 1.
+     */
+    public static int comparePowers(int[] number1, int[] number2) {
+        return (Double.compare(Math.log(number2[0]) * number2[1], Math.log(number1[0]) * number1[1]));
+    }
+
+    /**
+     * Squares individual digits of a number and concatenate them.
+     * @param n digit to square.
+     * @return squared and concatenated digit.
+     */
+    public int squareDigits(int n) {
+        int BASE = 10;
+
+        if (n < BASE) {
+            return n * n;
+        }
+        int digit = n % BASE;
+        int squaredDigit = digit * digit;
+        return squaredDigit + (squaredDigit < BASE ? BASE : BASE * BASE) * squareDigits(n / BASE);
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(order("is2 Thi1s T4est 3a"));
+        System.out.println("Available methods:");
+        System.out.println("1. Jaden Casing String." +
+                "2. Your order." +
+                "3. Ones and Zeros." +
+                "4. Two Sum." +
+                "5. Check square." +
+                "6. Rock, Paper, Scissors, Lizard, Spock. " +
+                "7. Compare Powers. " +
+                "8. Square Digits.");
+        System.out.println("Input the number of the method you would like to run. \n:");
+
+        int input = scanner.nextInt();
+
+        /*
+        String str;
+        switch (input){
+            case 1:
+                System.out.println("Input string to switch casing: ");
+                System.out.println(toJadenCase(scanner.nextLine()));
+                break;
+            case 2:
+                System.out.println("Input your order: ");
+                System.out.println(order(scanner.nextLine()));
+                break;
+            case 3:
+                System.out.println();
+                break;
+            case 4:
+                System.out.println("Input");
+        }
+
+         */
     }
 }
