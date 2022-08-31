fdf28e38ddddc53186efef12c241d0a81ca9604d
Kurt <73582015+abrycta@users.noreply.github.com>
2022-08-31 11:48:48 +0800

    Complete Permutations (4 Kyu)

--- MainProgram.java
+++ MainProgram.java
@@ -176,6 +176,32 @@ public class MainProgram {
     }
     
     // Complete Permutations: 4 Kyu
+        public static List<String> singlePermutations(String s) {
+        List<String> result = new ArrayList<>();
+        permute(0, new StringBuilder(s), result);
+        return result
+                .stream()
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    private static void permute(int l, StringBuilder sb, List<String> result) {
+        if (l == sb.length()) {
+            result.add(sb.toString());
+        } else {
+            for (int i = l; i < sb.length(); i++) {
+                swap(sb, i, l);
+                permute(l + 1, new StringBuilder(sb), result);
+                swap(sb, i, l);
+            }
+        }
+    }
+
+    private static void swap(StringBuilder sb, int i, int j) {
+        char t = sb.charAt(i);
+        sb.setCharAt(i, sb.charAt(j));
+        sb.setCharAt(j, t);
+    }
     
 
     public static void main(String[] args) {
